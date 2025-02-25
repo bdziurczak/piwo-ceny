8d7e467ac0a4816c30c5b9d2fd76f1a779f0e208
bdzIurczak <bbb05bart@gmail.com>
2025-02-25 16:02:13 +0100

    __ZabkaBeerScraper now returns data in the form of list of dictionaries.  Bug fixed: <br> tags inside <div> have been halting reading the rest of data in <div>

--- beer_data.py
+++ beer_data.py
@@ -12,13 +12,18 @@ class BeerData:
         self._paths = None
     async def pull(self):
         self._paths = await self.__get_paths()
-        self.zabka_beer_data = await self.__scrape_zabka_beer_data()
+        self.zabka_beer_data = await self.__extract_zabka_beer_data()
 
     @staticmethod
     async def __get_paths():
         with open("paths.json", "r", encoding="utf-8") as f:
             paths = json.load(f)
             return paths
+
+    async def __extract_zabka_beer_data(self):
+        raw = await self.__scrape_zabka_beer_data()
+        processed = []
+        return raw
 
     async def __scrape_zabka_beer_data(self):
         assert self._paths is not None
@@ -42,21 +47,38 @@ class BeerData:
             super().__init__()
             self.ZABKA_HTML_PATH = zabka_html_path
             self.read_mode = False
-            self.data = ""
+            self.data = []
+            self.data_record_buff = {}
+            self.class_buff = ""
             self.interesting_classes = ("product-item-content__title",
                                         "product-item-content__text product-item-content__informations",
                                         "product-label__text", "product-info__bottom-label")
         def handle_starttag(self, tag, attrs):
             attrs_dict = dict(attrs)
-            if "class" in attrs_dict and any(e in attrs_dict["class"] for e in self.interesting_classes):
+            if "class" in attrs_dict:
+                contained_interesting_classes = []
+                for e in self.interesting_classes:
+                    if e in attrs_dict["class"]:
+                        contained_interesting_classes.append(e)
                 self.read_mode = True
-            else:
-                self.read_mode = False
+                self.class_buff = ' '.join(contained_interesting_classes)
+
+                if contained_interesting_classes:
+                    #distinguishing different beers by a div with title
+                    if "product-item-content__title" in attrs_dict["class"]:
+                        self.data.append(self.data_record_buff) if self.data_record_buff else None
+                        self.data_record_buff = {} #clearing buffer
+                else:
+                    self.read_mode = False
 
         def handle_data(self, data):
             if self.read_mode:
-                self.data += data
+                tdata = data.strip()
+                if tdata:
+                    self.data_record_buff[self.class_buff] = tdata #avoid adding strings only with whitespaces
             pass
 
         def handle_endtag(self, tag):
+            if tag == "br":
+                self.read_mode = True
             pass
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from beer_data import BeerData
 async def main():
     data = BeerData()
     await data.pull() #populate BeerData instance
-    print(data.zabka_beer_data)
+    print(data.zabka_beer_data[1])
 if __name__ == '__main__':
     asyncio.run(main())
     #a
